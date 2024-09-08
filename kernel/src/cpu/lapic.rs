efeaebedc7d2abf0e2a7cefff0719a600ef7753f
raynamargham <rayanmargham4@gmail.com>
2024-09-08 10:16:58 +0100

    help again

--- kernel/src/cpu/lapic.rs
+++ kernel/src/cpu/lapic.rs
@@ -7,6 +7,8 @@ use super::CPU;
 pub trait LAPIC
 {
     unsafe fn ksleep(&self, ms: u64);
+    fn write_lapic_register(&self, reg: u64, val: u64);
+    fn read_lapic_register(&self, reg: u64) -> u64;
     fn init_lapic(&mut self, e: &mut ACPIMANAGER);
 }
 impl LAPIC for CPU
@@ -22,9 +24,29 @@ impl LAPIC for CPU
             
         }
     }
+    fn write_lapic_register(&self, reg: u64, val: u64)
+    {
+        
+        unsafe {
+            
+            core::ptr::write_volatile((self.lapic_addr + reg) as *mut u64, val);
+        }
+    }
+    fn read_lapic_register(&self, reg: u64) -> u64
+    {
+        
+        unsafe {
+           
+            
+           return core::ptr::read_volatile((self.lapic_addr + reg) as *mut u64);
+        }
+    }
     fn init_lapic(&mut self, e: &mut ACPIMANAGER) {
         let addr = rdmsr(0x1b);
-        self.lapic_addr = addr & 0xfffff000;
+        
+        self.lapic_addr = (addr & 0xfffff000) + HDDM_OFFSET.get_response().unwrap().offset();
+        // map lapic
+        unsafe {cur_pagemap.as_mut().unwrap().map(self.lapic_addr, addr & 0xffff000, VMMFlags::KTWRITEALLOWED.bits() | VMMFlags::KTPRESENT.bits()).unwrap()};
         println!("addr of lapic for cpu: {:#x}", self.lapic_addr);
         let table = e.0.find_table::<HpetTable>().unwrap_or_else(|_| panic!("fuck"));
         unsafe {
@@ -57,8 +79,25 @@ impl LAPIC for CPU
                 self.time_per_tick_hpet = it;
                 self.hpet_addr_virt = q.base_address as u64 + HDDM_OFFSET.get_response().unwrap().offset() as u64;
                 println!("tim : {it}");
-                self.ksleep(1000);
-                println!("woah");
+                // 0x100 enables the interrupt, 33 is the interrupt number for a surprious interrupt
+                self.write_lapic_register(0xf0, 0x100 | 33);
+                // divide by 4
+                self.write_lapic_register(0x3e0, 1);
+                // one shot timer, unmasked on interrupt 34
+                // 1 << 16 masks the timer
+                self.write_lapic_register(0x320, 34 | (1 << 16));
+                // calibrate the lapic, set the inital count
+                self.write_lapic_register(0x380, 0xffffffff);
+                self.ksleep(10);
+                let mut lapic_ticks_per_10ms = self.read_lapic_register(0x390);
+                lapic_ticks_per_10ms = 0xffffffff - lapic_ticks_per_10ms;
+                println!("lapic ticks per 10 ms {}", lapic_ticks_per_10ms);
+                self.write_lapic_register(0x380, lapic_ticks_per_10ms);
+                // unmasked, periodic, interrupt 34
+                // (1 << 17) sets to periodic
+                // read sdm for more info
+                self.write_lapic_register(0x320, 34 | (0 << 16) | (1 << 17));
+                
 
             }
             else {
